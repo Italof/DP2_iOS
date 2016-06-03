80cde9430ca4c55fabb681edce935b46771bffa5
joca <jpereyraleon@gmail.com>
2016-06-03 01:30:14 -0500

    UI testing in xcode

--- UASAppUITests/FacultiesTest.swift
+++ UASAppUITests/FacultiesTest.swift
@@ -1,18 +1,18 @@
 //
-//  UASAppUITests.swift
-//  UASAppUITests
+//  FacultiesTest.swift
+//  UASApp
 //
-//  Created by Karl Montenegro on 02/05/16.
+//  Created by José Carlos Pereyra on 6/3/16.
 //  Copyright © 2016 puntobat. All rights reserved.
 //
 
 import XCTest
 
-class UASAppUITests: XCTestCase {
-        
+class FacultiesTest: XCTestCase {
+    
     override func setUp() {
         super.setUp()
-        
+       
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -21,29 +21,14 @@ class UASAppUITests: XCTestCase {
         XCUIApplication().launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
         let device = XCUIDevice.sharedDevice()
         device.orientation = .LandscapeLeft
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testCantLoginWithNoData() {
-        
+    func login() {
         let app = XCUIApplication()
-        app.buttons["Iniciar Sesión"].tap()
-        let errorAlert = app.alerts["Error"]
-        
-        XCTAssert(errorAlert.exists)
-        
-    }
-    
-    func testCanLogin() {
-        
-        let app = XCUIApplication()
-        
+
         let usernameField = app.textFields["Usuario"]
         usernameField.tap()
         usernameField.typeText("admin")
@@ -59,9 +44,28 @@ class UASAppUITests: XCTestCase {
         
         
         app.buttons["Iniciar Sesión"].tap()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testCanLogout() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
         
-        let errorAlert = app.alerts["Error"]
-        XCTAssertFalse(errorAlert.exists)
+        login()
+        let app = XCUIApplication()
+        
+        app.navigationBars["Especialidades"].buttons["Cerrar Sesión"].tap()
+        
+        let okButton = app.alerts["Atención"].collectionViews.buttons["OK"]
+        okButton.tap()
+ 
+        let usernameField = app.textFields["Usuario"]
+        
+        XCTAssert(usernameField.exists)
         
     }
 
