b58e1e3f8acf2b111ac16ae317dc3ec8d3cbdd6b
karlmontenegro <karlmontenegrodj@gmail.com>
2016-06-24 11:16:42 -0500

    added suggestions

---
+++ DS_StudentResults.swift
@@ -0,0 +1,14 @@
+//
+//  DS_StudentResults.swift
+//  UASApp
+//
+//  Created by Karl Montenegro on 23/06/16.
+//  Copyright © 2016 puntobat. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DS_StudentResults {
+    
+}
